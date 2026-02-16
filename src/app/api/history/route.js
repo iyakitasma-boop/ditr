7cc1d08670eecc08e81a8e4d8028b5041e59749a
Ditzxxz <iyakitasma@gmail.com>
2026-02-16 17:02:06 +0900

    Update route.js

--- src/app/api/history/route.js
+++ src/app/api/history/route.js
@@ -2,7 +2,7 @@
 
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/libs/auth-options"
+import { authOptions } from "@/app/libs/auth-options";
 import prisma from "@/app/libs/prisma";
 
 export async function POST(request) {
